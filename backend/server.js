5a5e743d140dd52e1d9be9965b9679cede88c8bc
ac-animesh <animeshchandra01@gmail.com>
2022-09-17 20:59:56 +0530

    Added Multer for file upload

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,27 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const multer = require("multer");
 const connectDB = require("./config/db");
 
 app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 8000;
+
+// upload image
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "backend/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "_" + file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("myFile"), (req, res) => {
+  res.status(200).json({ message: "File has been Uploaded" });
+});
 
 app.use("/api/auth/", require("./routes/authRoutes"));
 app.use("/api/user/", require("./routes/usersRoutes"));
